b100b60d56d01b8484f0bc520911c59f106521ff
CodyMurr <codakmurraki@gmail.com>
2022-06-22 18:30:36 -0500

    add edit feature to profile

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -6,15 +6,20 @@ export default function Profile() {
   const { user, changeDetails, toggleEditDetails } = useContext(UserContext);
   return (
     <div className='flex flex-col justify-center items-start w-full h-96 text-primary-content'>
-      <h2 className='flex justify-between text-3xl font-extrabold w-2/12 my-5 mx-5'>
-        Your Details <FaRegEdit size={25} />
+      <h2 className='flex items-center text-3xl font-extrabold w-2/12 m-5'>
+        Your Details&nbsp;&nbsp;&nbsp;&nbsp;
+        <FaRegEdit
+          className='cursor-pointer'
+          size={25}
+          onClick={toggleEditDetails}
+        />
       </h2>
-      <form className='flex flex-col items-start h-full w-full'>
-        <label className='flex flex-col w-1/2 text-xl font-bold my-5 mx-5'>
+      <form className='flex flex-col items-center h-full w-full'>
+        <label className='flex flex-col w-full text-xl font-bold m-5'>
           Name:
           <div className='flex w-full items-center justify-between'>
             <input
-              className='input input-secondary w-4/5'
+              className='input input-secondary w-full'
               type='text'
               name='name'
               disabled={!changeDetails}
@@ -22,11 +27,11 @@ export default function Profile() {
             />
           </div>
         </label>
-        <label className='flex flex-col w-1/2 text-xl font-bold my-5 mx-5'>
+        <label className='flex flex-col w-full text-xl font-bold m-5'>
           Email:
           <div className='flex w-full items-center justify-between'>
             <input
-              className='input input-secondary w-4/5'
+              className='input input-secondary w-full'
               type='email'
               name='email'
               disabled={!changeDetails}
