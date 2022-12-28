6f3e8043089d426f6c80098be1b0efc30afe42cb
CodyMurr <codakmurraki@gmail.com>
2022-12-28 10:42:32 -0600

    render transaction details

--- src/components/Transaction.jsx
+++ src/components/Transaction.jsx
@@ -1,5 +1,41 @@
 import React from 'react';
 
 export default function Transaction({ transaction }) {
-	return <div>{transaction._id}</div>;
+	return (
+		<div className='w-full border-2 flex flex-col'>
+			<section className='w-1/2 flex'>
+				<h3 className='text-primary text-lg font-bold w-1/3 flex justify-start'>
+					ID:
+				</h3>
+				&nbsp;
+				<p className='w-1/3 flex justify-start'>{transaction._id}</p>
+			</section>
+			<section className='w-1/2 flex'>
+				<h3 className='text-primary text-lg font-bold w-1/3 flex justify-start'>
+					Type:
+				</h3>
+				&nbsp;
+				<p className='w-1/3 flex justify-start'>{transaction.type}</p>
+			</section>
+			<section className='w-1/2 flex'>
+				<h3 className='text-primary text-lg font-bold w-1/3 flex justify-start'>
+					Amount:
+				</h3>
+				&nbsp;
+				<p className='w-1/3 flex justify-start'>${transaction.amount}</p>
+			</section>
+			<section className='w-1/2 flex'>
+				<h3 className='text-primary text-lg font-bold w-1/3 flex justify-start'>
+					Payee:
+				</h3>
+				&nbsp;
+				<p className='w-1/3 flex justify-start'>
+					{transaction.payee
+						.split(' ')
+						.map((s) => s.replace(s[0], s[0].toUpperCase()))
+						.join(' ')}
+				</p>
+			</section>
+		</div>
+	);
 }
--- src/pages/TransactionsPage.jsx
+++ src/pages/TransactionsPage.jsx
@@ -2,6 +2,7 @@ import { useEffect, useContext, useRef, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import BudgetContext from '../context/Budgets/BudgetContext';
 import Transaction from '../components/Transaction';
+import PageHeader from '../components/custom/PageHeader';
 
 export default function TransactionsPage() {
 	const { showBudget, budgets } = useContext(BudgetContext);
@@ -26,10 +27,11 @@ export default function TransactionsPage() {
 	const transactions = currentBudget.transactions;
 
 	return (
-		<div>
+		<main className='w-10/12 flex flex-col relative'>
+			<PageHeader text={`Transactions for ${currentBudget.category}`} />
 			{transactions.map((t) => (
 				<Transaction transaction={t} key={t._id} />
 			))}
-		</div>
+		</main>
 	);
 }
